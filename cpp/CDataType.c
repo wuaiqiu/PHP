9387f09cf4b47591f8bb940d3076a2fbf217ccd0
wuaiqiu <209115576@qq.com>
2018-08-09 15:18:44 +0800

    20180809

--- cpp/CDataType.c
+++ cpp/CDataType.c
@@ -18,27 +18,11 @@
  *
  *
  *3.变量
- *	  auto : 默认存储类型，动态数据区 ,C++11变成自动推导类型,但不能用于函数传参,可以用模板函数代替
- * 还不能用于推导数组类型;
+ *	  auto : 默认存储类型，动态数据区 ,C++11变成自动推导类型,不能用于推导数组元素类型;
  *	  static : 静态数据区（只在本源文件起作用）
  *	  extern : 导出非静态全局变量与函数，注意只需声明即可,函数默认是extern属性
  *	  register : C++11被弃用，可以使用但不再具备任何实际含义
  *
- *
- *4.类型转换
- *	整型 --> 实型 : 整数部分不变，增加小数部分
- *	实型 --> 整型 : 整数部分不变，去除小数部分
- *	字符 --> 整型 : 高位为0，低8位为ascii码
- *	整型 --> 字符 : 低8位填充
- *
- *
- *5.类型判断(ctype.h)
- *	int isalnum(int c):该函数检查所传的字符是否是字母和数字。
- *	int isalpha(int c):该函数检查所传的字符是否是字母。
- *	int isdigit(int c):该函数检查所传的字符是否是十进制数字。
- *
- *	int tolower(int c):该函数把大写字母转换为小写字母。
- *	int toupper(int c):该函数把小写字母转换为大写字母。
  * */
 
 //2.cpp
@@ -60,19 +44,19 @@ int main(){
 
 
 /*
- * C++引用变量(给已定义的变量取一个别名，而typedef是给类型取一个别名):
+ *4.C++引用变量(给已定义的变量取一个别名，而typedef是给类型取一个别名):
  *	  a.基本变量引用(定义时必须初始化):
  *			int a=12;
  *			int &b=a; 
- *	  b.数组变量引用:
+ *	  b.数组变量引用(定义时必须初始化):
  *			int a[2];
  *			int (&b)[2]=a;
  *			int a[2][3];
  *			int (&b)[2][3]=a;
- *	  c.结构体引用:
+ *	  c.结构体引用(定义时必须初始化):
  *			Node a;
  *			Node &b=a;
- *	  d.指针引用:
+ *	  d.指针引用(定义时必须初始化):
  *			int* a;
  *			int* &b=a;
  *	  e.做函数参数
@@ -85,52 +69,80 @@ int main(){
 
 
 /*
- * C++新式类型转换(更加细致，更加安全),C++11不在使用传统的类型转换
- * 	 1.static_cast(与传统的强制转换相同):
- * 	 	a.用于类层次结构中父子类之间指针或引用的转换。向上安全；向下不安全。
- * 	 	b.用于基本数据类型之间的转换，如把int转换成char。
- * 	 	c.把空指针转换成目标类型的空指针。
- * 	 	d.把任何类型的表达式转换成void类型。
- *
- *			Type A = static_cast<Type>(B)
+ * 5.C++新式类型转换(更加细致，更加安全),C++11不在使用传统的类型转换
+ * 	 1.static_cast:
+ *	    a.编译时检查
+ * 	    b.用于类层次结构中父子类之间指针或引用的转换
+ * 	    c.用于基本数据类型之间的转换
+ * 	    d.把void指针与目标类型指针相互转换。
+ * 	    e.向下转不报错
  *
  *	 2.dynamic_cast:
- *	 	a.用于类层次结构中父子类之间指针或引用的转换。向上安全；向下安全。
- *
- *			Type A = dynamic_cast<Type>(B)
- *
+ *	    a.运行时检查
+ *	    b.目标必须时一个有效的指针，一个左值，一个右值
+ *	    c.向下转报错
+ *	   
  * 	 3.const_cast:
- *		a.常量指针被转化成非常量的指针，并且仍然指向原来的对象；
- *		b.常量引用被转换成非常量的引用，并且仍然指向原来的对象；
- *
- *			Type A = const_cast<Type>(B)
+ *	    a.常量指针(引用)被转化成非常量的指针(引用)；
  *
  * 	 4.reinterpret_cast:
- * 	 	a.它可以将任何指针转换为任何其他指针类型。
- * 	 	b.它可以把一个指针转换成一个整数，也可以把一个整数转换成一个指针。
- *
- * 	 		Type A = reinterpret_cast<Type>(B)
+ * 	    a.它可以将任何指针转换为任何其他指针类型。
+ * 	    b.它可以把一个指针转换成一个整数，也可以把一个整数转换成一个指针。
  * */
 
+class Person{};
+class Student : public Person{};
+
+int main(){
+	//1.将char型数据转换成int型数据
+	char a = 'a';
+	int b = static_cast<int>(a);
+	//2.将double指针转换成void指针
+	double* c = new double;
+	void* d = static_cast<void*>(c);
+	//3.将int型数据转换成const int型数据，但不能逆过来
+	int e = 10;
+	const int f = static_cast<const int>(e);
+	//4.将Student型数据转换成Person型数据
+	Student s;
+	Person p = static_cast<Person>(s);
+	return 0;
+}
+
+int main(){
+	//1.将Student型数据转换成Person型数据
+	Student* a = new Student;
+	Person* b = dynamic_cast<Person*>(a);
+	//2.报错
+	Person* c = new Person;
+	Student* d = dynamic_cast<Student*>(c);
+	return 0;
+}
+
+int main(){
+	//1.将const Student型数据转换成Student型数据
+	const Student* a = new Student;
+	Student* b = const_cast<Student*>(a);
+	return 0;
+}
+
+
 /*
- * C++11右值引用
+ * 6.C++11右值引用
  *	1.左值是可以出现=左侧者
  *	2.右值是只能出现=右侧者
  *	3.当右值赋值的过程中，执行拷贝内存与释放内存，而右值引用则直接移动内存
  **/
 
 //a.移动语义:将左值转换成右值
-void process(int& i)
-{
+void process(int& i){
     cout << "右值引用"  << endl;
 }
-void process(int&& i)//
-{
+void process(int&& i){
     cout << "左值引用"  << endl;
 }
 
-int main()
-{
+int main(){
   int a = 0;
   process(a); // 右值引用
   process(move(a)); // 左值引用
@@ -138,27 +150,22 @@ int main()
 }
 
 //b.完美转发:避免转发过程右值变成左值
-void process(int& i)
-{
+void process(int& i){
     cout << "右值引用"  << endl;
 }
-void process(int&& i)
-{
+void process(int&& i){
     cout << "左值引用"  << endl;
 }
-void not_forward(int&& i) 
-{
+void not_forward(int&& i){
     cout << "非完美转发"  << endl;
     process(i);
 }
-void per_forward(int&& i) 
-{
-	cout << "完美转发"  << endl;
+void per_forward(int&& i){
+    cout << "完美转发"  << endl;
     process(forward<int>(i));
 }
 
-int main()
-{
+int main(){
   int a = 0;
   not_forward(move(a)); // 右值引用
   per_forward(move(a)); // 左值引用
