9f65520801eda3b7689d0ae0cd51668db2e563ae
wuaiqiu <209115576@qq.com>
2018-08-16 11:01:28 +0800

    20180816

--- cpp/CException.c
+++ cpp/CException.c
@@ -51,3 +51,36 @@ int main(){
 	}
 	return 0;
 }
+
+/*
+ *C++11:
+ *  noexcept关键字:表示其修饰的函数不会抛出异常,若抛出了异常直接终止程序的运行
+ *
+ **/
+
+void Throw() { throw 1; }
+void NoBlockThrow() { Throw(); }
+void BlockThrow() noexcept { Throw(); }
+
+int main() {
+    try {
+        Throw();
+    }
+    catch(...) {
+        cout << "Found throw." << endl;     // Found throw.
+    }
+
+    try {
+        NoBlockThrow();
+    }
+    catch(...) {
+        cout << "Throw is not blocked." << endl;    // Throw is not blocked.
+    }
+
+    try {
+        BlockThrow();   // terminate called after throwing an instance of 'int'
+    }
+    catch(...) {
+        cout << "Found throw 1." << endl;
+    }
+}
